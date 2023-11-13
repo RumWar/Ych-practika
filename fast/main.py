f69bf74d716123b59f79629e44181c275291863f
RumWar <danila.yanvarev2004@gmail.com>
2023-11-13 17:14:07 +0300

    disain

--- fast/main.py
+++ fast/main.py
@@ -1,2 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
 # git remote add origin https: // github.com / RumWar / Ych - practika.git
 
+uvicorn.run(app)
